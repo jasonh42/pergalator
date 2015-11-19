39e1c1dfcff18b3c059b63862e2816ceab9a6a44
jhorsley <jason.h42@gmail.com>
2015-11-19 12:52:07 -0800

    Adding grid collection to homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,19 @@
 var Vue = require("vue");
 
+var pergs = [
+    {name: "Perg 1 "},
+    {name: "Perg 2"}
+]
+
 module.exports = {
     template: require("./home.html"),
     components: {
-        'discover': require('./../discover/discover.js')
+        'discover': require('./../discover/discover.js'),
+        "perg-collection": require("./../perg-collection/collection.js")
+    },
+    data: function() {
+        return {
+            pergs: pergs
+        }
     }
 };
---
+++ src/components/perg-collection/collection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    template: require("./collection.html"),
+    props: {
+        pergs: Array
+    }
+};
