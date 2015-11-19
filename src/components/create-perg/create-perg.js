3b4bcb9c6bfb127329b1be363024722d63919a22
Gregory Jeckell <gregory.jeckell@gmail.com>
2015-11-19 14:07:25 -0800

    Add some form controls

--- src/components/create-perg/create-perg.js
+++ src/components/create-perg/create-perg.js
@@ -1,11 +1,23 @@
 var Vue = require("vue");
-var validator = require('vue-validator')
-
-Vue.use(validator)
 
 require('./create-perg.scss');
 module.exports = {
     template: require("./create-perg.html"),
+    data: function() {
+      return {
+        perg: {
+          name: '',
+          city: '',
+          state: '',
+          imageURL: '',
+        },
+        user: {
+          firstName: '',
+          lastName: '',
+        },
+
+      }
+    },
     ready: function() {
     },
     methods: {
