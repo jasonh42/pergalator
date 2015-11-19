6297b09024af97f079c7d4bef9a843e74ce8d1e5
Ngoc Tran <ntran@pandora.com>
2015-11-19 11:48:31 -0800

    Added discover-components

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,8 @@
 var Vue = require("vue");
 
 module.exports = {
-    template: require("./home.html")
+    template: require("./home.html"),
+    components: {
+        'filters': require('./../discover/discover.js')
+    }
 };
