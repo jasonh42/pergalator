75a2084faa013c908bbf1cdd50dc34b84614c0c0
jhorsley <jason.h42@gmail.com>
2015-11-19 15:46:40 -0800

    Show/hide bindings for the filter button

--- src/components/discover/discover.js
+++ src/components/discover/discover.js
@@ -5,6 +5,11 @@ require('bootstrap-sass');
 module.exports = {
     template: require("./discover.html"),
     props: ['searchText'],
+    data: function() {
+        return {
+            showFilters: false
+        }
+    },
     components: {
         'filters': require('./filters/filters.js')
     }
