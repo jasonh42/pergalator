19189f0798f6e2452f2ae4ffec9d331f7defcad2
Ngoc Tran <ntran@pandora.com>
2015-11-18 16:21:39 -0800

    Added vue.js static directory

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/node_modules/vue/dist/'));
 
 var server = app.listen(3000, function() {
     var host = server.address().address;
