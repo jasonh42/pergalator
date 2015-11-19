5f771dc8df1b865f0060a2517d2ae204b8ad203a
Gregory Jeckell <gregory.jeckell@gmail.com>
2015-11-19 15:07:26 -0800

    Add additional input groups

--- src/components/create-perg/create-perg.js
+++ src/components/create-perg/create-perg.js
@@ -1,8 +1,9 @@
 var Vue = require("vue");
-
+require('bootstrap-sass');
 require('./create-perg.scss');
+
 module.exports = {
-    template: require("./create-perg.html"),
+    template: require('./create-perg.html'),
     data: function() {
       return {
         perg: {
@@ -14,10 +15,17 @@ module.exports = {
         user: {
           firstName: '',
           lastName: '',
+          email: '',
         },
+        locations: {},
       }
     },
     ready: function() {
+      this.$http.get('/locations.json', function(data) {
+          this.$set('locations', data);
+      }).error(function(resp) {
+          alert("Request loading");
+      });
     },
     methods: {
     }
