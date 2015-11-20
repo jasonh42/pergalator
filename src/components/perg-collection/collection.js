0ec04741f1e110d6e0740be0c4096bf3ab61e30d
Sebastian Ng <sng@pandora.com>
2015-11-19 16:21:33 -0800

    Added label for displaying status in grid item.

--- src/components/perg-collection/collection.js
+++ src/components/perg-collection/collection.js
@@ -2,5 +2,19 @@ require('./collection.scss');
 
 module.exports = {
     template: require("./collection.html"),
-    props: ['pergs', 'searchText']
+    props: ['pergs', 'searchText'],
+    methods: {
+        getStatusName: function(statusCode) {
+        	switch (statusCode) {
+        		case 0:
+            		return "Recruiting";
+        		case 1:
+            		return "Reviewing";
+        		case 2:
+            		return "Active";
+        		case 3:
+            		return "Inactive";
+        	}
+        }
+    }
 };
